c11d3d30d4bda17e3a39025b65bef84d9089f589
SYMMETRY4U <121978473+SYMMETRY4U@users.noreply.github.com>
2023-07-19 23:29:03 -0700

    HW3 forgot to commit

---
+++ models/credentials.js
@@ -0,0 +1,2 @@
+// For security, connectionString should be in a separate file and excluded from git
+export const connectionString = "mongodb+srv://dbuser:2679School@cluster0.hcfrcic.mongodb.net/?retryWrites=true&w=majority";
